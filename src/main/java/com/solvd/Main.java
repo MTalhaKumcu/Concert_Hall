460d249e9a0d39725a6563547cae01c4be19ba5a
=Talha Kumcu <=mkumcu151@gmail.com>
2024-01-06 00:56:05 +0100

    connection pool added

--- src/main/java/com/solvd/Main.java
+++ src/main/java/com/solvd/Main.java
@@ -7,17 +7,7 @@ public class Main {
 
     public static void main(String[] args) throws SQLException {
 
-        String sql = "SELECT * FROM  Concert_Hall.artists WHERE ArtistID=2;";
-        String url = "jdbc:mysql://localhost:3306/concert_hall";
-        String username ="root";
-        String password ="admin";
 
-        Connection connection = DriverManager.getConnection(url,username,password);
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery(sql);
-        resultSet.next();
-        String artistID = resultSet.getString(1);
-        System.out.println(artistID);
 
     }
 
--- src/main/java/com/solvd/persistence/connection/ConnectionPool.java
+++ src/main/java/com/solvd/persistence/connection/ConnectionPool.java
@@ -1,4 +1,22 @@
 package com.solvd.persistence.connection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class ConnectionPool {
+
+    // private static final String sql = "SELECT * FROM  Concert_Hall.artists WHERE ArtistID=2;";
+    private static final  String url = "jdbc:mysql://localhost:3306/concert_hall";
+    private static final  String username ="root";
+    private static final  String password ="admin";
+
+
+    Connection connection = DriverManager.getConnection(url,username,password);
+    Statement statement = connection.createStatement();
+    ResultSet resultSet = statement.executeQuery(sql);
+        resultSet.next();
+    String artistID = resultSet.getString(1);
+        System.out.println(artistID);
 }
