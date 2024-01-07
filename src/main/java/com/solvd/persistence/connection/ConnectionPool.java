1e33c3f1b5c8e2336795d52b9999ed5d31d63b6e
=Talha Kumcu <=mkumcu151@gmail.com>
2024-01-07 12:19:18 +0100

    connection pool config properties added

--- src/main/java/com/solvd/persistence/connection/ConnectionPool.java
+++ src/main/java/com/solvd/persistence/connection/ConnectionPool.java
@@ -1,6 +1,9 @@
 package com.solvd.persistence.connection;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.*;
+import java.util.Properties;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
@@ -10,35 +13,50 @@ public class ConnectionPool {
     private BlockingQueue<Connection> connectionPool;
     private static ConnectionPool instance;
 
-    public ConnectionPool(int connectionPoolSize) throws SQLException, InterruptedException {
+    private String url;
+    private String username;
+    private String password;
 
-        try {
+    public ConnectionPool(int connectionPoolSize) throws SQLException, InterruptedException {
+        this.connectionPool = new ArrayBlockingQueue<>(connectionPoolSize);
+
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream("config.properties") {
+            Properties properties = new Properties();
+            if (input == null) {
+                System.out.println("Sorry, unable to find config.properties");
+            }
+            properties.load(input);
+
+            url = properties.getProperty("db.url");
+            username = properties.getProperty("db.username");
+            password = properties.getProperty("db.password");
+
             for (int i = 0; i < connectionPoolSize; i++) {
-                Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/mydb", "root", "admin");
+                Connection connection = DriverManager.getConnection(url, username, password);
                 connectionPool.put(connection);
             }
-        } catch (SQLException | InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
 
-    public synchronized static ConnectionPool getInstance(int size) throws SQLException, InterruptedException {
-        if (instance == null) {
-            try {
-                instance = new ConnectionPool(size);
-            } catch (SQLException | InterruptedException e) {
-                e.printStackTrace();
+        } catch (IOException | SQLException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        public synchronized static ConnectionPool getInstance (int size) throws SQLException, InterruptedException {
+            if (instance == null) {
+                try {
+                    instance = new ConnectionPool(size);
+                } catch (SQLException | InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
+            return instance;
         }
-        return instance;
-    }
-    public Connection getConnection() throws InterruptedException {
-        return connectionPool.take();
-    }
-    public  void releaseConnection(Connection connection){
-        connectionPool.offer(connection);
-    }
+        public Connection getConnection () throws InterruptedException {
+            return connectionPool.take();
+        }
+        public void releaseConnection (Connection connection){
+            connectionPool.offer(connection);
+        }
 
+    }
 }
-
 
