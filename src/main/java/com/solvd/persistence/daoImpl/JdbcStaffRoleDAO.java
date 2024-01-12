0997a76a01947dc5d316eadeeda756f2da52161d
=Talha Kumcu <=mkumcu151@gmail.com>
2024-01-12 21:29:34 +0100

    jdbc fixed

--- src/main/java/com/solvd/persistence/daoImpl/JdbcStaffRoleDAO.java
+++ src/main/java/com/solvd/persistence/daoImpl/JdbcStaffRoleDAO.java
@@ -1,6 +1,9 @@
 package com.solvd.persistence.daoImpl;
 
+import com.solvd.model.Role;
+import com.solvd.model.Staff;
 import com.solvd.model.StaffRole;
+import com.solvd.persistence.connection.ConnectionPool;
 import com.solvd.persistence.dao.StaffRoleDAO;
 
 import java.sql.*;
@@ -8,19 +11,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class JdbcStaffRoleDAO implements StaffRoleDAO {
+    private final ConnectionPool connectionPool;
 
-    private Connection connection;
-
-    public JdbcStaffRoleDAO(Connection connection) {
-        this.connection = connection;
+    public JdbcStaffRoleDAO(ConnectionPool connectionPool) {
+        this.connectionPool = connectionPool;
     }
+
     @Override
-    public StaffRole getStaffRoleByID(int StaffRolesID) {
+    public StaffRole getStaffRoleByID(int staffRoleID) {
         StaffRole staffRole = null;
         String query = "SELECT * FROM StaffRoles WHERE StaffRoleID = ?";
 
-        try (PreparedStatement statement = connection.prepareStatement(query)) {
-            statement.setInt(1, StaffRolesID);
+        try (Connection connection = connectionPool.getConnection();
+             PreparedStatement statement = connection.prepareStatement(query)) {
+
+            statement.setInt(1, staffRoleID);
             ResultSet resultSet = statement.executeQuery();
 
             if (resultSet.next()) {
@@ -33,14 +38,13 @@ public class JdbcStaffRoleDAO implements StaffRoleDAO {
         return staffRole;
     }
 
-
-
     @Override
     public List<StaffRole> getAllStaffRoles() {
         List<StaffRole> staffRoles = new ArrayList<>();
         String query = "SELECT * FROM StaffRoles";
 
-        try (Statement statement = connection.createStatement();
+        try (Connection connection = connectionPool.getConnection();
+             Statement statement = connection.createStatement();
              ResultSet resultSet = statement.executeQuery(query)) {
 
             while (resultSet.next()) {
@@ -58,18 +62,20 @@ public class JdbcStaffRoleDAO implements StaffRoleDAO {
     public void addStaffRole(StaffRole staffRole) {
         String query = "INSERT INTO StaffRoles (StaffID, RoleID) VALUES (?, ?)";
 
-        try (PreparedStatement statement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
-            statement.setInt(1, staffRole.getStaffID());
-            statement.setInt(2, staffRole.getRoleID());
+        try (Connection connection = connectionPool.getConnection();
+             PreparedStatement statement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
+
+            statement.setInt(1, staffRole.getStaffID().getStaffID());
+            statement.setInt(2, staffRole.getRoleID().getRoleID());
 
             int affectedRows = statement.executeUpdate();
 
             if (affectedRows == 0) {
                 throw new SQLException("StaffRole creation failed, no rows affected.");
             }
-
             try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
                 if (generatedKeys.next()) {
+                    // Assuming StaffRoleID is an auto-generated key in the database
                     staffRole.setStaffRoleID(generatedKeys.getInt(1));
                 } else {
                     throw new SQLException("StaffRole creation failed, no ID obtained.");
@@ -84,9 +90,11 @@ public class JdbcStaffRoleDAO implements StaffRoleDAO {
     public void updateStaffRole(StaffRole staffRole) {
         String query = "UPDATE StaffRoles SET StaffID = ?, RoleID = ? WHERE StaffRoleID = ?";
 
-        try (PreparedStatement statement = connection.prepareStatement(query)) {
-            statement.setInt(1, staffRole.getStaffID());
-            statement.setInt(2, staffRole.getRoleID());
+        try (Connection connection = connectionPool.getConnection();
+             PreparedStatement statement = connection.prepareStatement(query)) {
+
+            statement.setInt(1, staffRole.getStaffID().getStaffID());
+            statement.setInt(2, staffRole.getRoleID().getRoleID());
             statement.setInt(3, staffRole.getStaffRoleID());
 
             statement.executeUpdate();
@@ -96,11 +104,13 @@ public class JdbcStaffRoleDAO implements StaffRoleDAO {
     }
 
     @Override
-    public void deleteStaffRole(int staffRolesID) {
+    public void deleteStaffRole(int staffRoleID) {
         String query = "DELETE FROM StaffRoles WHERE StaffRoleID = ?";
 
-        try (PreparedStatement statement = connection.prepareStatement(query)) {
-            statement.setInt(1, staffRolesID);
+        try (Connection connection = connectionPool.getConnection();
+             PreparedStatement statement = connection.prepareStatement(query)) {
+
+            statement.setInt(1, staffRoleID);
 
             statement.executeUpdate();
         } catch (SQLException e) {
@@ -108,11 +118,15 @@ public class JdbcStaffRoleDAO implements StaffRoleDAO {
         }
     }
 
-    private StaffRole mapResultSetToStaffRole(ResultSet resultSet) throws SQLException{
+    private StaffRole mapResultSetToStaffRole(ResultSet resultSet) throws SQLException {
         int staffRoleID = resultSet.getInt("StaffRoleID");
         int staffID = resultSet.getInt("StaffID");
         int roleID = resultSet.getInt("RoleID");
 
-        return new StaffRole(staffRoleID, staffID, roleID);
+        Staff staff = getStaffRoleByID(staffID).getStaffID();
+        Role role = getStaffRoleByID(roleID).getRoleID();
+
+        return new StaffRole(staffRoleID, staff, role);
+
     }
 }
