6f1706e4566b58af7855027970a2d5fc232c0752
=Talha Kumcu <=mkumcu151@gmail.com>
2024-01-19 00:58:59 +0100

    xml validator added

--- src/main/java/com/solvd/XMLValidator.java
+++ src/main/java/com/solvd/XMLValidator.java
@@ -1,5 +1,45 @@
 package com.solvd;
 
-public class XMLValidator {
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.xml.sax.SAXException;
 
+import javax.xml.XMLConstants;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import javax.xml.validation.Schema;
+import java.io.File;
+import java.io.IOException;
+
+public class XMLValidator {
+    private static final Logger LOGGER = LogManager.getLogger(Main.class);
+
+    public static boolean validateXmlAgainsrXsd(File xmlFile ,File xsdFile) throws IOException {
+        try{
+            SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+            Schema schema = schemaFactory.newSchema(xsdFile);
+
+            Validator validator = schema.newValidator();
+
+            validator.validate(new StreamSource(xmlFile));
+
+            return  true;
+        }catch (SAXException e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+    public static void main(String args[]) throws IOException {
+        File xmlFile = new File("src/main/resources/Concert_Hall.xml");
+        File xsdFile = new File("src/main/resources/Concert_hall.xsd");
+
+    if (validateXmlAgainsrXsd(xmlFile,xsdFile)){
+        LOGGER.info("XML File is avaliable for XSD File !");
+    }else {
+        LOGGER.info("XML File is not avaliable for XSD File !");
+
+    }
+
+    }
 }
